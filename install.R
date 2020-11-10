af8a720a5f77803b2b6333a057059c40164c95db
sheilarm23 <sheilaroma89@gmail.com>
2020-11-10 12:04:03 -0600

    for binder update

---
+++ install.R
@@ -0,0 +1,9 @@
+# Download needed packages
+
+install.packages("rentrez") # Download and install ...
+install.packages("ape") 
+install.packages("seqinr")
+install.packages("BiocManager")
+install.packages("phangorn")
+install.packages("systemPipeR")
+install.packages("ggtree")
