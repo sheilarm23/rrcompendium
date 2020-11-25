0091bd70bf69029858cda119e1325e2ce9814524
sheilarm23 <sheilaroma89@gmail.com>
2020-11-24 18:43:00 -0600

    removing paper.R from testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,9 +5,8 @@ library(knitr)
 library(testthat)
 
 knitr::knit('analysis/paper/paper.Rmd', tangle=TRUE)
-source('analysis/paper/paper.R')
 
 testthat::test_file('tests/testthat/test-gettingRF.R')
 testthat::test_file('tests/testthat/test-gettingBestModel.R')
 
-system('rm analysis/paper/paper.R')
+
