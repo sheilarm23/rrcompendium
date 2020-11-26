0e76505a4bd6115b25f14d7664dbc68d85f0092e
sheilarm23 <sheilaroma89@gmail.com>
2020-11-26 13:41:25 -0600

    travis5

--- tests/testthat/test-getBestModel.R
+++ tests/testthat/test-getBestModel.R
@@ -1,19 +1,14 @@
 
 context("test-getBestModel")
 
-test1 <- test_that("getBestModel errors out when a file of class other than 'DNAbin' is taken", {
-  expect_error(class(fastaRF)!= c("DNAbin"))
-})
+test_that("getBestModel", {
+  expect_is(getBestModel, "function")
 
-test2 <- test_that("getBestModel outputs an object of class modelTest", {
-  expect_is(class(getBestModel(fastaRF)), c("modelTest", "data.frame"))
-})
+  bestModel <- getBestModel(fastaRF)
+  expect_true(class(bestModel) == "data.frame")    # "getBestModel outputs an object of class data.frame"
+  expect_equal(nrow(bestModel), 24)  # getBestModel(fastaRF) has 24 rows based on the number of model selected"
+  expect_equal(bestModel$AICc[1], min(bestModel$AICc))  # getBestModel(fastaRF) has sorted the AICc column from lowest to highest values
 
-test3 <- test_that("getBestModel(fastaRF) has 24 rows based on the number of model selected", {
-  expect_equal(nrow(getBestModel(fastaRF)), 24)
-})
+  expect_error(getBestModel(fastaRF = c("ANTCATTTGA")))
 
-test4 <- test_that("getBestModel(fastaRF) has sorted the AICc column from lowest to highest value", {
-  expect_equal(getBestModel(fastaRF)$AICc[1], min(getBestModel(fastaRF)$AICc))
 })
-
