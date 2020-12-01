8b7c55bcf80027b1d431695338dc731fcfc2d577
sheilarm23 <sheilaroma89@gmail.com>
2020-11-30 23:21:56 -0600

    updating binder with BiocManager

--- install.R
+++ install.R
@@ -1,13 +1,15 @@
-# Installing packages for binder        # I did not include here "rmarkdown", "testthat", or any other because the project doesnt rely directly on them to run
+# Installing packages for binder        # I did not include here "rmarkdown", "testthat", or any other because the project doesn't rely directly on them to run
 install.packages("raster")
 install.packages("ggplot2")
 install.packages("ggthemes")
+install.packages("here")
 install.packages("rentrez")
 install.packages("ape")
 install.packages("seqinr")
+install.packages("phangorn")
+install.packages("BiocManager")
 install.packages("msa")
 install.packages("Biostrings")
 install.packages("systemPipeR")
-install.packages("phangorn")
 install.packages("ggtree")
 
