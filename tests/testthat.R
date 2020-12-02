cc94028800a13af599dce3a978bdfdca843d76a1
sheilarm23 <sheilaroma89@gmail.com>
2020-12-02 17:23:08 -0600

    almost done

--- tests/testthat.R
+++ tests/testthat.R
@@ -14,10 +14,7 @@ library(ape)
 library(phangorn)
 library(systemPipeR)
 
-#knitr::knit('analysis/paper/paper.Rmd', tangle=TRUE)
-
-knitr::knit('R/getBestModel.R', tangle=TRUE)
-knitr::knit('R/getRF.R', tangle=TRUE)
+knitr::knit('analysis/paper/paper.Rmd', tangle=TRUE)
 
 testthat::test_file('tests/testthat/test-getRF.R')
 testthat::test_file('tests/testthat/test-getBestModel.R')
