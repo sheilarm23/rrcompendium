6b732b4a11d18539d965f0a9bb7c5c3138832a6b
sheilarm23 <sheilaroma89@gmail.com>
2020-12-01 20:44:47 -0600

    still getBestModel

--- tests/testthat/test-getBestModel.R
+++ tests/testthat/test-getBestModel.R
@@ -2,20 +2,20 @@
 context("test-getBestModel")
 
 SeqA <- c("ANTCATTTGA")
-SeqB <- as.DNAbin("TAGTAGGTACTGCCC", "TAGTAGGTACTGCCC", "TAGTAGGTACTGCCC", "TAGTAGGTACTGCCC")
+SeqB <- as.DNAbin(c("TAGTAGGTACTGCCC", "TAGTAGGTACTGCCC", "TAGTAGGTACTGCCC", "TAGTAGGTACTGCCC"))
 
-test_that("getBestModel", {
-
-  #expect_is(getBestModel, "function")  # getBestModel is a function
-
-  expect_error(getBestModel(SeqA))     # getBestModel errors out when a file of class other than "DNAbin" is taken
-
-  bestModel <- getBestModel(SeqB)
-
-  expect_true(class(bestModel) == "data.frame")         # getBestModel outputs an object of class data.frame
-  expect_equal(nrow(bestModel), 24)                     # getBestModel output has 24 rows based on the number of model selected
-  expect_equal(bestModel$AICc[1], min(bestModel$AICc))  # getBestModel outputs a data.frame sorted from lowest to highest AICc values
-
+test1 <- test_that("getBestModel errors out when a file of class other than -DNAStringSet- is taken", {
+  expect_error(getBestModel(SeqA))
 })
 
+test2 <- test_that("getBestModel outputs an object of class data.frame", {
+  expect_true(class(getBestModel(SeqB)) == "data.frame")
+})
 
+test3 <- test_that("getBestModel output has 24 rows based on the number of model selected", {
+  expect_equal(nrow(getBestModel(SeqB)), 24)
+})
+
+test4 <- test_that("getBestModel outputs a data.frame sorted from lowest to highest AICc values", {
+  expect_equal(getBestModel(SeqB)$AICc[1], min(getBestModel(SeqB)$AICc))
+})
