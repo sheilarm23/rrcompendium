031e59d7eb969060445d7a2e103cad59f90c6584
sheilarm23 <sheilaroma89@gmail.com>
2020-12-02 09:33:29 -0600

    updating functions arguments

--- tests/testthat/test-getBestModel.R
+++ tests/testthat/test-getBestModel.R
@@ -12,14 +12,16 @@ test2 <- test_that("getBestModel", {
   expect_error(getBestModel(SeqA)) # errors out when a file of class other than -DNAStringSet- is taken
 })
 
+bestModel <- getBestModel(SeqB)
+
 test3 <- test_that("getBestModel", {
-  expect_true(class(getBestModel(SeqB)) == "data.frame")  # outputs an object of class data.frame
+  expect_true(class(bestModel) == "data.frame")  # outputs an object of class data.frame
 })
 
 test4 <- test_that("getBestModel", {
-  expect_equal(nrow(getBestModel(SeqB)), 24)   # output has 24 rows based on the number of model selected
+  expect_equal(nrow(bestModel), 24)   # output has 24 rows based on the number of model selected
 })
 
 test5 <- test_that("getBestModel", {
-  expect_equal(getBestModel(SeqB)$AICc[1], min(getBestModel(SeqB)$AICc))  # outputs a data.frame sorted from lowest to highest AICc values
+  expect_equal(bestModel$AICc[1], min(bestModel$AICc))  # outputs a data.frame sorted from lowest to highest AICc values
 })
--- tests/testthat/test-getRF.R
+++ tests/testthat/test-getRF.R
@@ -8,10 +8,10 @@ test1 <- test_that("getRF", {
   expect_is(getRF, "function")
 })
 
-test1 <- test_that("getRF", {
+test2 <- test_that("getRF", {
   expect_error(getRF(Seq1))   # errors out when a file of class other than -DNAStringSet- is taken
 })
 
-test2 <- test_that("getRF", {
+test3 <- test_that("getRF", {
   expect_error(getRF(Seq2))   #  errors out when an alignment includes gaps
 })
